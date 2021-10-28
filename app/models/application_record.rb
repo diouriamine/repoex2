b54f48c0515a7a92ae49f5f28d6f6349b80f5627
amine diouri <diouriamine@hotmail.com>
2021-10-28 09:03:24 +0100

    ex2

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,8 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 end
+# 1 ere étape  = creer les models
+# 2 e = rails db:migrate
+# 3e : validations
+# 4 : seeds (data) exemple on crée une applciation pour avoir des données fictives
+# 5 : routing + controller + view
